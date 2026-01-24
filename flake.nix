30f0a3f04818b7e389f3c36bf2e79c561b8afc9f
Hoodrich-Pablo-Juan <spoitorufane04@gmail.com>
2026-01-25 01:34:33 +0200

    Add zen-browser and firefox-addons to flake.nix

--- flake.nix
+++ flake.nix
@@ -8,12 +8,23 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    firefox-addons = {
+      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }: {
+  outputs = { self, nixpkgs, home-manager, zen-browser, firefox-addons, ... }: {
     nixosConfigurations = {
       dell-laptop = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
+        specialArgs = { inherit zen-browser firefox-addons; };
         modules = [
           ./hosts/dell-laptop/configuration.nix
           
@@ -23,6 +34,7 @@
             home-manager.useUserPackages = true;
             home-manager.backupFileExtension = "backup";
             home-manager.users.bryllm = import ./home;
+            home-manager.extraSpecialArgs = { inherit zen-browser firefox-addons; };
           }
         ];
       };
