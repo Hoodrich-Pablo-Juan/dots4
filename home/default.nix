c8953034817e71219d7b4b555991dd4dce7c462b
Hoodrich-Pablo-Juan <spoitorufane04@gmail.com>
2026-01-25 02:17:38 +0200

    Update default.nix

--- home/default.nix
+++ home/default.nix
@@ -1,6 +1,7 @@
 { config, pkgs, zen-browser, firefox-addons, ... }:
 
 {
+  # Import modular configs
   imports = [
     ./packages.nix
     ./firefox.nix
@@ -9,12 +10,16 @@
     ./waybar
   ];
 
+  # User info
   home.username = "bryllm";
   home.homeDirectory = "/home/bryllm";
   home.stateVersion = "25.11";
 
   programs.home-manager.enable = true;
 
+  # -------------------------------
+  # Bash
+  # -------------------------------
   programs.bash = {
     enable = true;
     profileExtra = ''
@@ -25,101 +30,24 @@
     '';
   };
 
-  programs.kitty = {
-    enable = true;
-    settings = {
-      background_opacity = "0.78";
-      background_blur = 64;
-      font_family = "JetBrainsMono Nerd Font";
-      font_size = "11.0";
-      window_padding_width = 16;
+  # -------------------------------
+  # Terminals
+  # -------------------------------
+  programs.kitty.enable = true;
+  programs.alacritty.enable = true;
 
-      foreground = "#333333";
-      background = "#CCCCCC";
-      selection_foreground = "#CCCCCC";
-      selection_background = "#333333";
-      cursor = "#333333";
-      cursor_text_color = "#CCCCCC";
-
-      color0 = "#333333";
-      color8 = "#474747";
-      color1 = "#474747";
-      color9 = "#5A5A5A";
-      color2 = "#474747";
-      color10 = "#5A5A5A";
-      color3 = "#474747";
-      color11 = "#5A5A5A";
-      color4 = "#474747";
-      color12 = "#5A5A5A";
-      color5 = "#474747";
-      color13 = "#5A5A5A";
-      color6 = "#474747";
-      color14 = "#5A5A5A";
-      color7 = "#AFAFAF";
-      color15 = "#FFFFFF";
-    };
-  };
-
-  programs.alacritty = {
-    enable = true;
-    settings = {
-      window = {
-        opacity = 0.78;
-        blur = true;
-        padding = { x = 16; y = 16; };
-        decorations = "Full";
-      };
-      font = {
-        normal = {
-          family = "JetBrainsMono Nerd Font Mono";
-          style = "Regular";
-        };
-        size = 12.0;
-      };
-      colors = {
-        primary = {
-          background = "0xCCCCCC";
-          foreground = "0x333333";
-        };
-        normal = {
-          black = "0x333333";
-          red = "0x474747";
-          green = "0x474747";
-          yellow = "0x474747";
-          blue = "0x474747";
-          magenta = "0x474747";
-          cyan = "0x474747";
-          white = "0xAFAFAF";
-        };
-        bright = {
-          black = "0x474747";
-          red = "0x5A5A5A";
-          green = "0x5A5A5A";
-          yellow = "0x5A5A5A";
-          blue = "0x5A5A5A";
-          magenta = "0x5A5A5A";
-          cyan = "0x5A5A5A";
-          white = "0xFFFFFF";
-        };
-      };
-    };
-  };
-
+  # -------------------------------
+  # GTK / QT
+  # -------------------------------
   gtk = {
     enable = true;
-    theme = {
-      name = "Adwaita";
-      package = pkgs.gnome-themes-extra;
-    };
-    iconTheme = {
-      name = "Adwaita";
-      package = pkgs.adwaita-icon-theme;
-    };
-    cursorTheme = {
-      name = "Bibata-Modern-Classic";
-      package = pkgs.bibata-cursors;
-      size = 24;
-    };
+    theme.name = "Adwaita";
+    theme.package = pkgs.gnome-themes-extra;
+    iconTheme.name = "Adwaita";
+    iconTheme.package = pkgs.adwaita-icon-theme;
+    cursorTheme.name = "Bibata-Modern-Classic";
+    cursorTheme.package = pkgs.bibata-cursors;
+    cursorTheme.size = 24;
     gtk3.extraConfig.gtk-application-prefer-dark-theme = false;
     gtk4.extraConfig.gtk-application-prefer-dark-theme = false;
   };
@@ -138,36 +66,58 @@
     x11.enable = true;
   };
 
-  services.dunst = {
+  # -------------------------------
+  # Notifications
+  # -------------------------------
+  services.dunst.enable = true;
+
+  # -------------------------------
+  # Wallpaper
+  # -------------------------------
+  home.file.".config/wallpapers/wallpaper.jpg".source =
+    ../wallpapers/eink.jpg;
+
+  # -------------------------------
+  # Firefox / Zen Browser
+  # -------------------------------
+  programs.firefox.enable = true;
+
+  # If you want Zen Browser instead of Firefox, enable it here:
+  programs.zen-browser = {
     enable = true;
-    settings = {
-      global = {
-        width = 300;
-        height = 300;
-        offset = "30x50";
-        origin = "top-right";
-        transparency = 10;
-        frame_color = "#CCCCCC";
-        font = "JetBrainsMono Nerd Font 10";
-      };
-      urgency_low = {
-        background = "#CCCCCC";
-        foreground = "#474747";
-        timeout = 5;
-      };
-      urgency_normal = {
-        background = "#CCCCCC";
-        foreground = "#333333";
-        timeout = 10;
-      };
-      urgency_critical = {
-        background = "#CCCCCC";
-        foreground = "#000000";
-        timeout = 0;
+    package = zen-browser.packages.${pkgs.system}.twilight;
+
+    profiles.default = {
+      id = 0;
+      name = "default";
+      isDefault = true;
+
+      extensions = with firefox-addons.packages.${pkgs.system}; [
+        leechblock-ng
+        vimium-c
+        stylus
+      ];
+
+      settings = {
+        "privacy.donottrackheader.enabled" = true;
+        "privacy.trackingprotection.enabled" = true;
+        "privacy.trackingprotection.socialtracking.enabled" = true;
+
+        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+
+        "browser.newtabpage.activity-stream.feeds.telemetry" = false;
+        "browser.ping-centre.telemetry" = false;
+        "browser.tabs.crashReporting.sendReport" = false;
+        "datareporting.healthreport.uploadEnabled" = false;
+        "datareporting.policy.dataSubmissionEnabled" = false;
+        "toolkit.telemetry.enabled" = false;
+        "toolkit.telemetry.unified" = false;
+        "toolkit.telemetry.archive.enabled" = false;
+
+        "browser.newtabpage.enabled" = false;
+        "browser.startup.page" = 3;
+        "general.smoothScroll" = true;
       };
     };
   };
-
-  home.file.".config/wallpapers/wallpaper.jpg".source =
-    ../wallpapers/eink.jpg;
 }
