c034a1b04dd17a59ac6f152aa2b7c7a2c3606fcf
Hoodrich-Pablo-Juan <spoitorufane04@gmail.com>
2026-01-25 02:21:40 +0200

    Update default.nix

--- home/default.nix
+++ home/default.nix
@@ -1,7 +1,6 @@
 { config, pkgs, zen-browser, firefox-addons, ... }:
 
 {
-  # Import modular configs
   imports = [
     ./packages.nix
     ./firefox.nix
@@ -10,16 +9,12 @@
     ./waybar
   ];
 
-  # User info
   home.username = "bryllm";
   home.homeDirectory = "/home/bryllm";
   home.stateVersion = "25.11";
 
   programs.home-manager.enable = true;
 
-  # -------------------------------
-  # Bash
-  # -------------------------------
   programs.bash = {
     enable = true;
     profileExtra = ''
@@ -30,15 +25,9 @@
     '';
   };
 
-  # -------------------------------
-  # Terminals
-  # -------------------------------
   programs.kitty.enable = true;
   programs.alacritty.enable = true;
 
-  # -------------------------------
-  # GTK / QT
-  # -------------------------------
   gtk = {
     enable = true;
     theme.name = "Adwaita";
@@ -66,58 +55,12 @@
     x11.enable = true;
   };
 
-  # -------------------------------
-  # Notifications
-  # -------------------------------
   services.dunst.enable = true;
 
-  # -------------------------------
   # Wallpaper
-  # -------------------------------
   home.file.".config/wallpapers/wallpaper.jpg".source =
     ../wallpapers/eink.jpg;
 
-  # -------------------------------
-  # Firefox / Zen Browser
-  # -------------------------------
-  programs.firefox.enable = true;
-
-  # If you want Zen Browser instead of Firefox, enable it here:
-  programs.zen-browser = {
-    enable = true;
-    package = zen-browser.packages.${pkgs.system}.twilight;
-
-    profiles.default = {
-      id = 0;
-      name = "default";
-      isDefault = true;
-
-      extensions = with firefox-addons.packages.${pkgs.system}; [
-        leechblock-ng
-        vimium-c
-        stylus
-      ];
-
-      settings = {
-        "privacy.donottrackheader.enabled" = true;
-        "privacy.trackingprotection.enabled" = true;
-        "privacy.trackingprotection.socialtracking.enabled" = true;
-
-        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
-
-        "browser.newtabpage.activity-stream.feeds.telemetry" = false;
-        "browser.ping-centre.telemetry" = false;
-        "browser.tabs.crashReporting.sendReport" = false;
-        "datareporting.healthreport.uploadEnabled" = false;
-        "datareporting.policy.dataSubmissionEnabled" = false;
-        "toolkit.telemetry.enabled" = false;
-        "toolkit.telemetry.unified" = false;
-        "toolkit.telemetry.archive.enabled" = false;
-
-        "browser.newtabpage.enabled" = false;
-        "browser.startup.page" = 3;
-        "general.smoothScroll" = true;
-      };
-    };
-  };
+  # ✅ Zen Browser is included only via home.packages
+  # (remove any programs.zen-browser block)
 }
